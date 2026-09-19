1454ec395a10137cb14d0b13df742cce30cc8462
dsdfcs <3208174306@qq.com>
2026-09-19 18:30:53 +0800

    Refactor __init__.py to include new models
    Removed old model imports and added new credibility models.

--- __init__.py
+++ __init__.py
@@ -1,14 +1,11 @@
-# models/__init__.py
-from .customized_modeling_t5 import CustomizedT5ForConditionalGeneration
-from .customized_modeling_t5_rats import T5ForConditionalGenerationRATS
-from .customized_modeling_bart import BartForConditionalGeneration
-from .customized_modeling_pegasus import PegasusForConditionalGeneration
-from .customized_modeling_led import LEDForConditionalGeneration
+"""CrediCap: credibility-aware image-caption evaluation."""
+
+from credicap.evidence_decomposition import IntegratedEvidenceScorer
+from credicap.reference_credibility import ReferenceCredibilityModel
+from credicap.score_correction import DirectionMagnitudeCorrector
 
 __all__ = [
-    'CustomizedT5ForConditionalGeneration',
-    'T5ForConditionalGenerationRATS',
-    'BartForConditionalGeneration',
-    'PegasusForConditionalGeneration',
-    'LEDForConditionalGeneration',
+    "ReferenceCredibilityModel",
+    "IntegratedEvidenceScorer",
+    "DirectionMagnitudeCorrector",
 ]
